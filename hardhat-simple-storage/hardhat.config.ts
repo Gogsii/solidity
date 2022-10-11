593615ee450329f71bcefacb33a1955dc5a14cf3
Gogsii <dragutinovic.g@gmail.com>
2022-10-11 08:33:48 -0700

    added testing functionality

--- hardhat-simple-storage/hardhat.config.ts
+++ hardhat-simple-storage/hardhat.config.ts
@@ -1,8 +1,42 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { task, HardhatUserConfig } from "hardhat/config"
+import "@nomiclabs/hardhat-waffle"
+import "@nomicfoundation/hardhat-toolbox"
+import "dotenv/config"
+import "./tasks/block-number"
+import "hardhat-gas-reporter"
+import "@typechain/hardhat"
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https:eth-goerli/example"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKET_API_KEY || "key"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+    solidity: "0.8.17",
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY!],
+            chainId: 5,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            // accounts: hardhat does it for me
+            chainId: 31337,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        //token: "MATIC" to see how much it costs on other networks
+    },
+}
 
-export default config;
+export default config
--- hardhat-simple-storage/scripts/deploy.ts
+++ hardhat-simple-storage/scripts/deploy.ts
@@ -5,26 +5,60 @@
 // const fs = require("fs-extra");
 // require("dotenv").config();
 
-import { ethers } from "hardhat";
+import { ethers, run, network } from "hardhat"
+import "dotenv/config"
+import "@nomiclabs/hardhat-etherscan"
 
 // async main
 
 async function main() {
+    // We get the contract to deploy
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+    console.log("Deploying contract...")
 
-  // We get the contract to deploy
-  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
-  console.log("Deploying contract...")
-  const simpleStorage = await SimpleStorageFactory.deploy()
-  await simpleStorage.deployed()
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+    console.log("Simple Storage deployed to:", simpleStorage.address)
 
-  console.log("Simple Storage deployed to:", simpleStorage.address)
+    //if on testnet, verify contract
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for block txes ... ")
+        await simpleStorage.deployTransaction.wait(6) //wait six blocks before verifying below
+        await verify(simpleStorage.address, [])
+    }
 
+    //Get current value
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current value is: ${currentValue}`)
+    //Update current value
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated value is: ${updatedValue}`)
+}
+
+const verify = async (contractAddress: string, args: any[]) => {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e: any) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified!")
+        } else {
+            console.log(e)
+        }
+    }
 }
 
 // call main
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
