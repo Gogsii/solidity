bc37fd66e7ab56628e15683e1e65b98fef177239
Gogsii <dragutinovic.g@gmail.com>
2022-10-18 20:38:50 -0700

    removing fund me folder for now

--- hardhat-fund-me/hardhat.config.ts
+++
@@ -1,9 +0,0 @@
-import { HardhatUserConfig } from "hardhat/config"
-import "hardhat-deploy"
-import "@nomicfoundation/hardhat-toolbox"
-
-const config: HardhatUserConfig = {
-    solidity: "0.8.17",
-}
-
-export default config
